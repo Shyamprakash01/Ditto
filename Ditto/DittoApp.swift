bc80d590bf32c6d180f349dceef61d30cc8334eb
shyam <pshyam506@gmail.com>
2026-04-06 16:22:40 +0530

    Initial Commit

---
+++ Ditto/DittoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittoApp.swift
+//  Ditto
+//
+//  Created by Shyamprakash A on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DittoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
